5d3b93dedf30e0ab37e6f50e1a23244680e02530
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-22 20:36:52 +0100

    advanced extensions done

--- weekend_hw/db/cinema.sql
+++ weekend_hw/db/cinema.sql
@@ -1,4 +1,5 @@
 DROP TABLE tickets;
+DROP TABLE showtimes;
 DROP TABLE films;
 DROP TABLE customers;
 
@@ -12,11 +13,25 @@ CREATE TABLE customers(
 CREATE TABLE films(
   id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
-  price INT4
+  price INT4,
+  number_of_tickets INT4
 );
+
+CREATE TABLE showtimes(
+  id SERIAL4 PRIMARY KEY,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  showtime VARCHAR(255),
+  max_capacity INT4
+);
+
+-- CREATE TABLE tickets(
+--   id SERIAL4 PRIMARY KEY,
+--   customer_id INT4  REFERENCES customers(id) ON DELETE CASCADE,
+--   film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+-- );
 
 CREATE TABLE tickets(
   id SERIAL4 PRIMARY KEY,
   customer_id INT4  REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+  showtime_id INT4 REFERENCES showtimes(id) ON DELETE CASCADE
 );
--- weekend_hw/db/console.rb
+++ weekend_hw/db/console.rb
@@ -2,12 +2,14 @@ require('pry')
 require_relative('../model/film.rb')
 require_relative('../model/customer.rb')
 require_relative('../model/tickets.rb')
+require_relative('../model/showtime.rb')
 
 require( 'pry-byebug' )
 
 Film.delete_all()
 Customer.delete_all()
 Ticket.delete_all()
+Showtime.delete_all()
 
 customer1 = Customer.new({
   'name' => 'Andrew Bradley','funds' => 200})
@@ -36,27 +38,71 @@ film3.save()
 film3.price = 20
 film3.update
 
+showtime1 = Showtime.new({'film_id' => film1.id, 'showtime' =>'20:00', 'max_capacity' => 50 })
 
-tickets1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+showtime1.save()
+
+showtime2 = Showtime.new({'film_id' => film3.id, 'showtime' =>'17:00', 'max_capacity' => 40  })
+showtime2.save()
+
+showtime3 = Showtime.new({'film_id' => film1.id, 'showtime' =>'16:00', 'max_capacity' => 50 })
+showtime3.save()
+
+showtime4 = Showtime.new({'film_id' => film2.id, 'showtime' =>'11:00', 'max_capacity' => 50 })
+showtime4.save()
+
+showtime2.showtime = '23:00'
+showtime2.update
+
+
+tickets1 = Ticket.new({'customer_id' => customer1.id, 'showtime_id' => showtime1.id})
 tickets1.save()
 
-tickets2 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
+tickets2 = Ticket.new({'customer_id' => customer1.id, 'showtime_id' => showtime3.id})
 tickets2.save()
 
-tickets3 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film3.id})
+tickets3 = Ticket.new({'customer_id' => customer2.id, 'showtime_id' => showtime2.id})
 tickets3.save()
 
-tickets4 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film1.id})
+tickets4 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime1.id})
 tickets4.save()
 
-tickets5 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film1.id})
+tickets5 = Ticket.new({'customer_id' => customer2.id, 'showtime_id' => showtime1.id})
 tickets5.save()
 
-tickets6 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film3.id})
+tickets6 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime2.id})
 tickets6.save()
 
-tickets7 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film2.id})
+tickets7 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime1.id})
 tickets7.save()
+
+tickets15= Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime2.id})
+tickets15.save()
+
+tickets8 = Ticket.new({'customer_id' => customer2.id, 'showtime_id' => showtime3.id})
+tickets8.save()
+
+tickets9 = Ticket.new({'customer_id' => customer1.id, 'showtime_id' => showtime1.id})
+tickets9.save()
+
+tickets10 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime2.id})
+tickets10.save()
+
+tickets11 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime2.id})
+tickets11.save()
+
+tickets12 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime1.id})
+tickets12.save()
+
+tickets13 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime2.id})
+tickets13.save()
+
+tickets14 = Ticket.new({'customer_id' => customer3.id, 'showtime_id' => showtime3.id})
+tickets13.save()
+
+
+
+
 
 
 
